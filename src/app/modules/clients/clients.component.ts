6b73b73c5c812a2110123718b8e3b6dd06663997
Elusai Soares <elusaisoares@gmail.com>
2020-12-21 19:00:57 -0300

    listagem de clientes

--- src/app/modules/clients/clients.component.ts
+++ src/app/modules/clients/clients.component.ts
@@ -1,24 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { delay } from 'rxjs/operators';
+
 import { FakeBackendService } from 'src/app/core/services/fake-backend.service';
 import { ClientModel } from '../new-client/client-model';
+
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
   styleUrls: ['./clients.component.css']
 })
-export class ClientsComponent implements OnInit {
-
+export class ClientsComponent implements OnInit, AfterViewInit  {
+  displayedColumns: string[] = [
+    'select',
+    'nome',
+    'email',
+    'cpfOuCnpj',
+    'telefone',
+    'cep',
+    'logradouro',
+    'numero',
+    'bairro',
+    'cidade',
+    'estado'
+  ];
+  displayedColumnsNames: string[] = [
+    'Nome',
+    'Email',
+    'CPF/CNPJ',
+    'Telefone',
+    'CEP',
+    'Logradouro',
+    'Número',
+    'Bairro',
+    'Cidade',
+    'Estado'
+  ];
+  dataSource: MatTableDataSource<ClientModel>;
+  selection = new SelectionModel<ClientModel>(true, []);
   constructor(
     private fakeBackendService: FakeBackendService,
   ) {
-
   }
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource();
+    this.getClients();
+  }
+
+  ngAfterViewInit() {
+    // this.getData();
+  }
+
+  getClients() {
     this.fakeBackendService.getClients().subscribe((clients: ClientModel[]) => {
       console.log(clients);
+      this.dataSource.data = clients;
     });
+  }
+
+  deleteClients() {
+    console.log(this.selection.selected);
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: ClientModel): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.nome + 1}`;
   }
 
 
--- src/app/modules/clients/clients.module.ts
+++ src/app/modules/clients/clients.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsComponent } from './clients.component';
@@ -10,6 +14,10 @@ import { ClientsComponent } from './clients.component';
   declarations: [ClientsComponent],
   imports: [
     CommonModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
     ClientsRoutingModule
   ]
 })
